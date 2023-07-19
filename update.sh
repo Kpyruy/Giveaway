0526fa4b07dc513598ae3476137133d52d0dc8ca
Kpyruy <san3k0l0@gmail.com>
2023-07-19 19:59:54 +0300

    Update

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Удаляем папку Giveaway, если она существует
-rm -r Giveaway
-
-# Клонируем репозиторий
-git clone https://Kpyruy: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@github.com/Kpyruy/Giveaway
