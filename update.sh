5d48f904f5213ff389ff8daa56ad7440f76b2119
Kpyruy <105604635+Kpyruy@users.noreply.github.com>
2023-07-19 19:33:55 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@
 rm -r Giveaway
 
 # Клонируем репозиторий
-git clone https://Kpyruy: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@github.com/Kpyruy/Giveaway
+git clone https://Kpyruy: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@github.com/Kpyruy/Giveaway
